594417e678dd03e6550f8a2e3057a1d47c0d2b07
Davi Cardoso <davicardoso524@academico.ufs.br>
2026-04-16 06:42:10 -0300

    Update prisma configuration for Turso

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaLibSql } from '@prisma/adapter-libsql';
+import { createClient } from '@libsql/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const adapter = new PrismaLibSql({
-  url: 'file:./dev.db',
+const url = process.env.TURSO_DATABASE_URL || 'file:./dev.db';
+
+const libsql = createClient({
+  url,
+  authToken: process.env.TURSO_AUTH_TOKEN,
 });
+
+const adapter = new PrismaLibSql(libsql);
 
 export const prisma =
   globalForPrisma.prisma ||
